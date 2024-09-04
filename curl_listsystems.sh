973d668f56acb16f79f453d623fb7ffdbe07c26d
doccaz <erico.mendonca@gmail.com>
2024-09-03 23:46:54 -0300

    adding curl examples

---
+++ curl_listsystems.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+API="https://susemanager.suselab.localdomain/rhn/manager/api"
+POSTDATA='{"login": "infobot", "password": "infobot321"}'
+CMD="curl -LSks -c cookies.txt -b cookies.txt -H @headers.txt"
+
+# faz o login
+$CMD -d "${POSTDATA}" ${API}/auth/login
+if [ $? -eq 0 ]; then
+	echo
+	echo "autenticado com sucesso"
+	echo
+
+	# executamos o comando...
+	OUTPUT=$($CMD ${API}/system/listSystems)
+
+	# fazemos o logout
+	# o POST data pode ser vazio, pois ele desloga usando os cookies
+	$CMD -d "" ${API}/auth/logout
+	if [ $? -eq 0 ]; then
+		echo
+		echo "logout com sucesso"
+		echo
+	else
+		echo "erro no logout"
+		exit 1
+	fi
+else
+	echo "erro ao autenticar"
+	exit 1
+fi
+
+
+echo "saida: $OUTPUT"
