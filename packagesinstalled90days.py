cf0748098d10034969d9a08fe8d4408f7f5ed324
doccaz <erico.mendonca@gmail.com>
2024-09-05 10:41:47 -0300

    script was crashing on non-package related events

--- packagesinstalled90days.py
+++ packagesinstalled90days.py
@@ -16,7 +16,7 @@ def main():
             session_key = proxy.auth.login(MANAGER_USER, MANAGER_PASS)
             print(f'system_id,system_name,hostname,ip_address,event_id,earliest_action,pickup_date,completed_date,status,event,event_data,synced_date')
             date_now = datetime.today()
-            package_actions = ['Package Install', 'Package Upgrade', 'Package Removal']
+            package_actions = ['Package Install', 'Package Upgrade', 'Package Removal', 'Patch Update']
             for s in proxy.system.listSystems(session_key):
                 details = proxy.system.getDetails(session_key, s['id'])
                 network = proxy.system.getNetwork(session_key, s['id'])
@@ -29,11 +29,12 @@ def main():
                             result = 'Completed'
                         else:
                             result = 'Failed'
-                        
+                        # print(f"--> found event: {p['action_type']}")
                         package_list = []
-                        for pkg in p['additional_info']:
-                            package_list.append(pkg['detail'])
-                        print(f"{s['id']},{details['profile_name']},{network['hostname']},{network['ip']},{p['id']},{p['earliest_action']},{p['pickup_date']},{p['completed_date']},{result},{p['action_type']},{';'.join(package_list)},{p['modified_date']}")        
+                        if 'additional_info' in p.keys():
+                            for pkg in p['additional_info']:
+                                package_list.append(pkg['detail'])
+                            print(f"{s['id']},{details['profile_name']},{network['hostname']},{network['ip']},{p['id']},{p['earliest_action']},{p['pickup_date']},{p['completed_date']},{result},{p['action_type']},{';'.join(package_list)},{p['modified_date']}")        
                 
             if (session_key) is not None:
                 proxy.auth.logout(session_key)
